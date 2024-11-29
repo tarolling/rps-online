01c3b28156d60b31aa0fa08fef48a384ab50fef5
tarolling <benitojoens@gmail.com>
2024-11-29 16:03:18 -0600

    what this

--- api/initPlayer.js
+++ api/initPlayer.js
@@ -14,14 +14,12 @@ export default async function handler(req, res) {
     } catch (err) {
         console.error(`Connection error\n${err}\nCause: ${err.cause}`)
         await driver.close()
-        return
+        return res.status(503).json({ error: 'Unable to connect to Neo4j' })
     }
 
     session = driver.session({ database: 'neo4j' })
     const username = await session.executeWrite(async tx => {
-        let result
-
-        result = await tx.run(`
+        let result = await tx.run(`
         MERGE (p:Player {id: $id, username: $username})
         ON CREATE
             SET p.created = timestamp()
@@ -33,15 +31,20 @@ export default async function handler(req, res) {
         )
 
         if (!result || result.records.length === 0) {
-            res.status(503)
-            throw new Error('Unable to modify player.')
+            return Promise.reject('Unable to modify player.')
         }
         return result.records[0].get('username')
     });
 
-    console.log(`User ${username} added!`)
+    // check if promise was rejected
+    if (username instanceof String) {
+        console.log(`User ${username} added!`)
+        res.status(200).json();
+    } else {
+        console.error('Unable to modify player.');
+        res.status(503).json({ error: 'Unable to modify player.' });
+    }
 
     await session.close()
     await driver.close()
-    res.status(200);
 }
