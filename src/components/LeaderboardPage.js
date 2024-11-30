24812263d8b3dfa5a89ad37ff0b7ecdeea0bb443
tarolling <benitojoens@gmail.com>
2024-11-29 20:05:08 -0600

    i hate async

--- src/components/LeaderboardPage.js
+++ src/components/LeaderboardPage.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 
-async function LeaderboardPage() {
+function LeaderboardPage() {
     const [playerData, setPlayerData] = useState(null);
 
     useEffect(() => {
