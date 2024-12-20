8ac42db432d49f9c936c83ebf0377c7aaa48b0e0
tarolling <benitojoens@gmail.com>
2024-12-19 23:37:44 -0500

    change to POST

--- api/checkUsername.js
+++ api/checkUsername.js
@@ -1,7 +1,7 @@
 import neo4j from "neo4j-driver";
 
 export default async function handler(req, res) {
-    if (req.method !== 'GET') {
+    if (req.method !== 'POST') {
         return res.status(405).json({ error: 'Method not allowed' });
     }
 
