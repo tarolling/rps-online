7fc934e226c14e7179266a8c615b3bb138dc8b0a
tarolling <benitojoens@gmail.com>
2024-12-20 14:02:16 -0500

    huh

--- api/checkUsername.js
+++ api/checkUsername.js
@@ -33,6 +33,8 @@ export default async function handler(req, res) {
             return result;
         });
 
+        console.log(`bro what ${read.records}`);
+
         if (read.records) {
             res.status(200).json({ usernameExists: true });
         } else {
