4235b26fa65f12d07a4ab1f74aa808d0269d8907
tarolling <benitojoens@gmail.com>
2024-12-22 16:58:03 -0600

    add analytics and adjust db calls

--- api/fetchPlayer.js
+++ api/fetchPlayer.js
@@ -15,7 +15,7 @@ export default async function handler(req, res) {
         }
 
         driver = neo4j.driver(process.env.NEO4J_URI, neo4j.auth.basic(process.env.NEO4J_USERNAME, process.env.NEO4J_PASSWORD))
-        await driver.getServerInfo()
+        await driver.getServerInfo();
     } catch (err) {
         console.error(`Connection error\n${err}\nCause: ${err.cause}`)
         if (driver) await driver.close();
@@ -27,18 +27,17 @@ export default async function handler(req, res) {
         const read = await session.executeRead(async tx => {
             let result = await tx.run(`
             MATCH (p:Player {uid: $uid})
-            RETURN p
+            RETURN p.username AS username, p.rating AS rating
             `, { uid: uid });
 
             if (result?.records.length === 0) {
                 throw new Error("No players with the specified user ID exists.");
             }
-            console.log(`RECORDS: ${JSON.stringify(result.records)}`);
 
             return result.records[0];
         });
 
-        res.status(200).json(read);
+        res.status(200).json({ username: read.get("username"), rating: read.get("rating") });
     } catch (err) {
         console.error('Error executing query:', err);
         res.status(500).json({ error: 'Failed to fetch player info.' });
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
+import { Analytics } from "@vercel/analytics/react";
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
 import App from './App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { sendToVercelAnalytics } from './vitals';
 
 const root = createRoot(document.getElementById('root'));
-root.render(<React.StrictMode><App /></React.StrictMode>)
+root.render(<React.StrictMode><App /><Analytics /></React.StrictMode>)
 
 reportWebVitals(sendToVercelAnalytics);
