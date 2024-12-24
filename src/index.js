d6727a4d8d570087fb9abfbd366626a7c125d40e
tarolling <benitojoens@gmail.com>
2024-12-23 22:01:56 -0600

    change analytics location

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/react";
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
 
 const root = createRoot(document.getElementById('root'));
-root.render(<React.StrictMode><App /><Analytics /><SpeedInsights /></React.StrictMode>)
+root.render(<React.StrictMode><App /></React.StrictMode>)
