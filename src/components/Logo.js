481fe307667e71c713c02b41b3af0010c87f22f5
tarolling <benitojoens@gmail.com>
2024-12-28 22:48:36 -0600

    fix #9

--- src/components/Logo.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import logo from '../assets/logo.png';
-
-function Logo() {
-    return <img src={logo} alt="RPS Logo" style={{ width: '150px', marginBottom: '20px' }} />;
-}
-
-export default Logo;
