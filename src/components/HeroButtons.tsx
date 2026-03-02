2516cac2f3466b4f691aa21a11f819f26f74d61e
tarolling <benitojoens@gmail.com>
2026-03-02 04:45:41 +0000

    front page redesign

--- src/components/HeroButtons.tsx
+++ src/components/HeroButtons.tsx
@@ -3,10 +3,10 @@ import Link from "next/link";
 import styles from "@/app/page.module.css";
 
 export default function HeroButtons() {
-    return (
-        <div className={styles.heroButtons}>
-            <Link href="/rules" className={styles.secondaryButton}>Learn the Rules</Link>
-            <Link href="/dashboard" className={styles.ctaButton}>Play Now</Link>
-        </div>
-    );
+  return (
+    <div className={styles.heroButtons}>
+      <Link href="/rules" className={styles.secondaryButton}>Learn the Rules</Link>
+      <Link href="/dashboard" className={styles.ctaButton}>Play Now</Link>
+    </div>
+  );
 }
